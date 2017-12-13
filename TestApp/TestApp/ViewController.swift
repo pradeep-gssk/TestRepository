a4e028f2ffbff135a728530a572bf723bd82d654
Krishna Pradeep <gss_krishnapradeep@yahoo.com>
2017-12-12 23:30:17 -0800

    test3

--- TestApp/TestApp/ViewController.swift
+++ TestApp/TestApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after the view, typically from a.
+        // Do any additional setup the view, typically from a.
     }
 
     override func didReceiveMemoryWarning() {
